2b5bb477342677ecec1d041be4de0009112da0a2
rogerthatdev <roger.that.dev@gmail.com>
2023-04-12 10:10:29 -0700

    add a boilerplate for PR trigger

--- terraform/modules/exp/main.tf
+++ terraform/modules/exp/main.tf
@@ -1,7 +1,8 @@
 locals {
   repository_name   = split("/", replace(var.github_repository_url, "/(.*github.com/)/", ""))[1]
   repository_owner  = split("/", replace(var.github_repository_url, "/(.*github.com/)/", ""))[0]
-  test_build_config = yamldecode(templatefile("${path.module}/build/test.cloudbuild.yaml", {}))
+  test_build_config = yamldecode(templatefile("${path.module}/cloudbuild/test.cloudbuild.yaml", {}))
+  pr_build_config   = yamldecode(templatefile("${path.module}/cloudbuild/pr.cloudbuild.yaml", {}))
 }
 
 output "test" {
@@ -38,3 +39,33 @@ resource "google_cloudbuild_trigger" "test_project_capture" {
 }
 
 
+resource "google_cloudbuild_trigger" "new_pr" {
+  project     = var.project_id
+  location    = var.region
+  name        = "test-new-pr"
+  description = ""
+
+  github {
+    name  = local.repository_name
+    owner = local.repository_owner
+    pull_request {
+      branch = "main"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+      invert_regex = false
+    }
+  }
+
+  build {
+    images        = []
+    substitutions = {}
+    tags          = []
+    dynamic "step" {
+      for_each = local.pr_build_config.steps
+      content {
+        args = step.value.args
+        name = step.value.name
+      }
+    }
+  }
+  substitutions = {}
+}
